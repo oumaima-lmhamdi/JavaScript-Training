6175ea767ebf573b06c8aef96bb37f29ce6e90f1
oumaima-lm <oumaima.lmhamdi@gmail.com>
2021-06-02 17:57:23 +0100

    Find a task with keyup event

--- TodaysTasks/todaystasks.js
+++ TodaysTasks/todaystasks.js
@@ -1,12 +1,13 @@
 const addTask = document.querySelector('.addTask');
 const taskList = document.querySelector('.todolist');
 const liTask = document.querySelector('li');
+const searchBar = document.querySelector('.searchBar input');
 
 
-// Add task
+//Add task
 
 const Add = () =>{
-    
+
     const li = document.createElement('li');
     li.textContent = addTask.add.value.trim();
     if(li.textContent.length){
@@ -33,14 +34,14 @@ const Add = () =>{
     
 };
 
-
+//Submit event
 addTask.addEventListener('submit', e=>{
     e.preventDefault();
 
     Add();    
 });
 
-// delete or mark tasks as done
+// Delete or mark tasks as done
 
 taskList.addEventListener('click', e=>{
     if(e.target.classList.contains('delete')){
@@ -53,3 +54,25 @@ taskList.addEventListener('click', e=>{
         taskList.append(e.target.parentElement.parentElement);    
     } 
 });
+
+//Find a task
+
+const findTask = (term)=>{
+    //Hide a task
+    Array.from(taskList.children)
+    .filter(task =>!task.textContent.toLowerCase().includes(term))
+    .forEach(task => task.classList.add('d-none'))
+
+    //Display a task
+    Array.from(taskList.children)
+    .filter(task =>task.textContent.toLowerCase().includes(term))
+    .forEach(task => task.classList.remove('d-none'))
+
+};
+
+//Keyup event
+searchBar.addEventListener('keyup', ()=>{
+    const term = searchBar.value.trim().toLowerCase();
+    findTask(term);
+});
+
