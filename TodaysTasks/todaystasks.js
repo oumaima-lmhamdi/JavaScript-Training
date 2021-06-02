fd4e789b853b95dc0579d9c84ef9740d792b4fbb
oumaima-lm <oumaima.lmhamdi@gmail.com>
2021-06-02 13:41:59 +0100

    delete make as done and updating submit events

--- TodaysTasks/todaystasks.js
+++ TodaysTasks/todaystasks.js
@@ -1,28 +1,55 @@
 const addTask = document.querySelector('.addTask');
 const taskList = document.querySelector('.todolist');
+const liTask = document.querySelector('li');
 
-const taskTemplate = task =>{
-    const template = 
-    `
-    <li class="list-group-item d-flex justify-content-between align-items-center text-light">
-        <span>${task}</span>
+
+// Add task
+
+const Add = () =>{
+    
+    const li = document.createElement('li');
+    li.textContent = addTask.add.value.trim();
+    if(li.textContent.length){
+
+        taskList.prepend(li);
+        let classesToAdd = ['list-group-item', 'd-flex', 'justify-content-between', 'align-items-center', 'text-light'];
+        li.classList.add(...classesToAdd);
+        addTask.reset();
+
+        const template = 
+        `
         <div>
             <i class="far fa-trash-alt delete"></i>
             <i class="far fa-check-circle done"></i>
         </div>
-    </li>
-    `;
 
-    taskList.innerHTML+= template;
+        `;
+
+        li.innerHTML+= template;
+
+        console.log(li.textContent);
+
+    }
+    
 };
+
 
 addTask.addEventListener('submit', e=>{
     e.preventDefault();
 
-    const task = addTask.add.value.trim();
+    Add();    
+});
 
-    if(task.length){
-        taskTemplate(task);
-        addTask.reset();
+// delete or mark tasks as done
+
+taskList.addEventListener('click', e=>{
+    if(e.target.classList.contains('delete')){
+        e.target.parentElement.parentElement.remove();
     }
+
+    if(e.target.classList.contains('done')){
+        e.target.parentElement.parentElement.style.textDecoration = 'line-through solid rgb(255, 255, 255)';
+        e.target.parentElement.parentElement.style.background = 'pink';
+        taskList.append(e.target.parentElement.parentElement);    
+    } 
 });
